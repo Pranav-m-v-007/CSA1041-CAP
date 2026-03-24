e042ae6c7f074528fd5a356097f133f2cae069e5
Pranav-m-v-007 <mvpranav11@gmail.com>
2026-03-24 09:39:55 +0530

    Update response message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD Working Successfully! HEllo Guys');
+  res.send('CI/CD Working Successfully! Nammathaaa');
 });
 
 app.listen(3000);
